4dcaf3dbc0c6dfa58b0c77eb14f023be7513fa97
Wellington Henrique <wellingtonhlc@gmail.com>
2022-06-06 08:56:51 -0300

    Getting students

--- src/pages/Alunos/index.jsx
+++ src/pages/Alunos/index.jsx
@@ -1,18 +1,60 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import logoCadastro from "../../assets/cadastro-ico.jpg"
+import { ToastContainer, toast } from 'react-toastify';
 
 import "./styles.css";
 import { FiXCircle, FiEdit, FiUserX } from "react-icons/fi";
+import api from "../../services/api";
 
 export default function Alunos() {
+    const  [nome, setNome] = useState('');
+    const[alunos, setAlunos] = useState([]);
+
+    const email = localStorage.getItem('email');
+    const token = localStorage.getItem('token');
+
+    const navigate = useNavigate();
+
+    const authorization = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+
+    useEffect(() => {
+        api.get('https://localhost:44311/api/alunos', authorization).then((response) => {
+            setAlunos(response.data);
+        })
+    })
+
+    async function logout() {
+        try {
+            localStorage.clear();
+            localStorage.setItem('token','');
+            authorization.headers = '';
+            navigate("/");
+        } catch(err) {
+            toast.error('erro: ' + err, {
+                position: "top-right",
+                theme: "colored",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: false,
+            });
+        }
+    }
+
     return (
         <div className="aluno-container">
             <header>
                 <img src={logoCadastro} alt="Cadastro" />
-                <span>Bem-vindo <strong>Desenvolvedor</strong>!</span>
+                <span>Bem-vindo <strong>{email}</strong>!</span>
                 <Link  className="button" to="/aluno/novo/0">Novo Aluno</Link>
-                <button type="button" className="">
+                <button type="button" className="" onClick={logout}>
                     <FiXCircle size={35} color="#17202a"/>
                 </button>
             </header>
@@ -25,18 +67,33 @@ export default function Alunos() {
             </form>
             <h1>Relação de alunos</h1>
             <ul>
-                <li>
-                    <b>Nome:</b>{}<br/><br/>
-                    <b>Email:</b>{}<br/><br/>
-                    <b>Idade:</b>{}<br/><br/>
+                {alunos.map(aluno => 
+                    <li>
+                    <b>Nome:</b>{aluno.nome}<br/><br/>
+                    <b>Email:</b>{aluno.email}<br/><br/>
+                    <b>Idade:</b>{aluno.idade}<br/><br/>
                     <button type="button" className="">
                         <FiEdit size={35} color="#17202a"/>
                     </button>
                     <button type="button" className="">
                         <FiUserX size={35} color="#17202a"/>
                     </button>
-                </li>
+                    </li>
+                )}
             </ul>
+
+            <ToastContainer
+                position="top-right"
+                theme="colored"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
         </div>
     )
 }
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,19 +1,82 @@
-import React from 'react';
-import './styles.css';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import api from "../../services/api";
 import logoImg from '../../assets/login.jpg';
+import { ToastContainer, toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
+import './styles.css';
 
 export default function Login () {
+    const[email, setEmail] = useState('');
+    const[password, setPassword] = useState('');
+
+    const navigate = useNavigate();
+
+    async function Login(e) {
+        e.preventDefault()
+
+        const data = {
+            email, password
+        }
+
+        try {
+            const response = await api.post('https://localhost:44311/api/account/loginuser', data);
+
+            localStorage.setItem('email', email);
+            localStorage.setItem('token', response.data.token);
+            localStorage.setItem('expiration', response.data.expiration);
+
+            navigate("/alunos");
+        } catch (err) {
+            //alert(`Falha: ${err.message}`);
+
+            toast.error('Não foi possível efetuar o login!', {
+                position: "top-right",
+                theme: "colored",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: false,
+            });
+        }
+    }
+
     return (
         <div className="login-container">
             <section className="form">
                 <img src={logoImg} alt="Login" id="img1"/>
-                <form>
+                <form onSubmit={Login}>
                     <h1>Cadastro de Alunos</h1>
-                    <input type="text" placeholder='Email'/>
-                    <input type="password" id="password" placeholder='Password'/>
+
+                    <input placeholder='Email' 
+                        value={email} 
+                        onChange={e => setEmail(e.target.value)}
+                    />
+
+                    <input type="password" placeholder='Password'
+                        value={password} 
+                        onChange={e => setPassword(e.target.value)}
+                    />
+
                     <button className="button" type="submit">Login</button>
                 </form>
             </section>
+
+            <ToastContainer
+                position="top-right"
+                theme="colored"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
         </div>
     )
 }
